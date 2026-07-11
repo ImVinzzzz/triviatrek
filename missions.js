4edde11cec7367ee2dfd1566ea842e5e45695cf2
ImVinzzzz <vricci75@gmail.com>
2026-07-11 16:20:33 +0200

    Update missions.js

--- missions.js
+++ missions.js
@@ -2,6 +2,6 @@
 // Aggiungi o rimuovi le missioni in questo elenco per caricarle nel menu iniziale del gioco.
 const MISSIONS = [
   { name: "Generico", file: "quiz.json" },
-  { name: "Afrotite 2", file: "afrodite-2.json" }
+  { name: "Afrotite 2", file: "afrodite-2.json" },
   { name: "Mimmo", file: "mimmo.json" }
 ];
