f50aaf4ed283211655afe190f2e5e8a3f2438f40
ImVinzzzz <vricci75@gmail.com>
2026-09-11 12:00:41 +0200

    Update missions.js

--- missions.js
+++ missions.js
@@ -3,5 +3,6 @@
 const MISSIONS = [
   { name: "Generico", file: "quiz.json" },
   { name: "Giuliano", file: "giuliano.json" },
+  { name: "Giuliano 2 - la Vendemmia", file: "giuliano2.json" },
   { name: "Afrotite 2", file: "afrodite-2.json" }
 ];
